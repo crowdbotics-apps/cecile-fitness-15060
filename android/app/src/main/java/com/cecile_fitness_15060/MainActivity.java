6b61bf17176093777b3853a142c1d0c717122812
Crowdbotics <team@crowdbotics.com>
2020-03-25 16:30:50 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cecile_fitness_15060/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cecile_fitness_15060;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "cecile_fitness_15060";
+    }
+}
